86e262f340f492d579130a6b93807f5a2ad66ff9
saurabh123r <100188659+saurabh123r@users.noreply.github.com>
2023-02-10 00:44:42 +0530

    Add files via upload

---
+++ services.js
@@ -0,0 +1,30 @@
+/*function myFunc() {
+         document.getElementById("full").style.display = "block";
+      document.getElementById("demo").style.display = "flex";
+      document.getElementById("ful").style.display = "block";
+      }
+ function myFun() {
+      document.getElementById("full").style.display = "none";
+      document.getElementById("demo").style.display = "flex";
+      document.getElementById("ful").style.display = "none";
+      }*/
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+     /* <div class="=sbtnparent" >
+      <button type="button" class="sbtn"   onclick="myFunc()"> Show all services </button>
+      <button type="button" class="sbtn" id="ful"  onclick="myFun()"> Hide all services </button>
+      </div>/*
---
+++ wb.js
@@ -0,0 +1,139 @@
+function myFunc() {
+         document.getElementById("demo").style.color = "purple";
+      document.getElementById("demo").style.backgroundColor = "black";
+      }
+      <nav class="form">
+      <div>
+      Download computer application
+   /*   </div>
+      <br>
+      <div>
+      <button  class="from"  ><a href="apps/_Getintopc.com_Windows_7_Super_Lite_Edition_Version_2017_86_Bit.iso">Download wondows 7 super lite</a></button>
+      </div>
+      <br>
+      <div>
+      <button class="from" ><a href="apps/windows 8.1.iso" class="fa fa-youtube" >Download windows 8.1</a></button>
+      </div> 
+      <br>
+      <div>
+      <button class="from" ><a href="apps/Firefox Installer.exe">Download fire fox</a></button>
+      </div>
+      <br> 
+      <div>
+      <button class="from" ><a href="apps/VSCodeUserSetup-ia32-1.64.2.exe">Download vs code</a></button>
+      </div> 
+      <br>
+      <div>
+      <button class="from" ><a href="apps/vlc-3.0.16-win32.exe">Download vlc media player</a></button>
+      </div> 
+      <br>
+      <div>
+      <button class="from" ><a href="apps/ChromeSetup.exe">Download google chrome</a></button>
+      </div> 
+      <br>
+      <div>
+      <button class="from" ><a href="apps/_Getintopc.com_Microsoft_Office_2007_Enterprise_Edition_2007.iso">Download ms office 2007 enterprise</a></button>
+      </div> 
+      <br>
+      <div>
+      <button class="from" ><a href="apps/Sonma-Typing-Expert-2.01.0000.exe">Download sonama tping</a></button>
+      </div> 
+      <br>
+      <div>
+      <button class="from" ><a href="apps/TypingMaster11.exe">Download typing master 11</a></button>
+      </div> 
+      <br>
+      <div>
+      <button class="from" ><a href="apps/rufus-3.18.exe">Download rufus-3.1</a></button>
+      </div> 
+      <br>
+      <div>
+      <button class="from" ><a href="apps/playit.exe">Download playit for windows</a></button>
+      </div> 
+      <br>
+      <div>
+      <button class="from" ><a href="apps/wintousb-6-6.exe">Download hasalo wintousb</a></button>
+      </div> 
+      <br>
+      <div>
+      <button class="from" ><a href="apps/Windows_Loader_v.2.2.2.rar">Download windows 7 activater </a></button>
+      </div> 
+      <br>
+      <div>
+      <button class="from" ><a href="apps/winrar-x32-611.exe">Download winrar exe </a></button>
+      </div> 
+      <br>
+      <div>
+      <button class="from" ><a href="apps/WinArchiver4.exe">Download WinArchiver4 exe </a></button>
+      </div> 
+      </nav>
+      </div>
+      <div class="flex-download-board">
+      <nav class="form">
+      Download android apps from my web site
+      <br>
+      <div>
+      <button class="from" ><a href="apps/_Getintopc.com_Windows_7_Super_Lite_Edition_Version_2017_86_Bit.iso">Download wondows 7 super lite</a></button>
+      </div>
+      <br>
+      <div>
+      <button class="from" ><a href="apps/windows 8.1.iso">Download windows 8.1</a></button>
+      </div> 
+      <br>
+      <div>
+      <button class="from" ><a href="apps/Firefox Installer.exe">Download fire fox</a></button>
+      </div>
+      <br> 
+      <div>
+      <button class="from" ><a href="apps/VSCodeUserSetup-ia32-1.64.2.exe">Download vs code</a></button>
+      </div> 
+      <br>
+      <div>
+      <button class="from" ><a href="apps/vlc-3.0.16-win32.exe">Download vlc media player</a></button>
+      </div> 
+      <br>
+      <div>
+      <button class="from" ><a href="apps/ChromeSetup.exe">Download google chrome</a></button>
+      </div> 
+      <br>
+      <div>
+      <button class="from" ><a href="apps/_Getintopc.com_Microsoft_Office_2007_Enterprise_Edition_2007.iso">Download ms office 2007 enterprise</a></button>
+      </div> 
+      <br>
+      <div>
+      <button class="from" ><a href="apps/Sonma-Typing-Expert-2.01.0000.exe">Download sonama tping</a></button>
+      </div> 
+      <br>
+      <div>
+      <button class="from" ><a href="apps/TypingMaster11.exe">Download typing master 11</a></button>
+      </div> 
+      <br>
+      <div>
+      <button class="from" ><a href="apps/rufus-3.18.exe">Download rufus-3.1</a></button>
+      </div> 
+      <br>
+      <div>
+      <button class="from" ><a href="apps/playit.exe">Download playit for windows</a></button>
+      </div> 
+      <br>
+      <div>
+      <button class="from" ><a href="apps/wintousb-6-6.exe">Download hasalo wintousb</a></button>
+      </div> 
+      <br>
+      <div>
+      <button class="from" ><a href="apps/Windows_Loader_v.2.2.2.rar">Download windows 7 activater </a></button>
+      </div> 
+      <br>
+      <div>
+      <button class="from" ><a href="apps/winrar-x32-611.exe">Download winrar exe </a></button>
+      </div> 
+      <br>
+      <div>
+      <button class="from" ><a href="apps/WinArchiver4.exe">Download WinArchiver4 exe </a></button>
+      </div> 
+      <br>
+      <div>
+      <button class="from" ><a href="apps/FreeFire.apk">Download Download 
+      Free fire </a></button>
+      </div> 
+      </nav>
